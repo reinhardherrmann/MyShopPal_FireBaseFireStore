cf3c5adb09df868530b9c224b1bd73b164559913
RHerrmann <reinhard-ulrich.herrmann@web.de>
2022-02-25 17:34:45 +0100

    step 12 splashscreen mit Wechsel zur Mainactivity nach 2,5 Sek. Ordner assets angelegt, 2 fonts hinterlegt BoldTextView hinzugefügt, assets-Folder erstellt 2 Fonts im assetsFolder gespeichert

---
+++ app/src/main/java/de/orome/myshoppal/BoldTextView.kt
@@ -0,0 +1,19 @@
+package de.orome.myshoppal
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class BoldTextView(context: Context,
+                   attributeSet: AttributeSet): AppCompatTextView(context,attributeSet) {
+    init{
+        applyFont()
+    }
+
+    private fun applyFont(){
+        val boldTypeface: Typeface =
+            Typeface.createFromAsset(context.assets,"Montserrat-Bold.ttf")
+        typeface = boldTypeface
+    }
+}
